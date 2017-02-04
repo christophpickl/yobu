eed176830c83380e1a707eadc67c07e57fe5270e
Christoph Pickl <christoph.pickl@gmail.com>
2017-02-04 18:01:56 +0100

    basic list view

--- app/src/main/kotlin/yobu/christophpickl/github/com/yobu/MainActivity.kt
+++ app/src/main/kotlin/yobu/christophpickl/github/com/yobu/MainActivity.kt
@@ -2,24 +2,37 @@ package yobu.christophpickl.github.com.yobu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import com.pawegio.kandroid.find
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private val LOG = LOG(javaClass)
+    }
+
     private val questions = QuestionRepo()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        LOG.i("onCreate(..)")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val txtOutput = find<TextView>(R.id.txtOutput)
         val btnNextQuestion = find<Button>(R.id.btnNextQuestion)
+        val answersList = find<ListView>(R.id.answersList)
+
         btnNextQuestion.setOnClickListener {
-            println("btnNextQuestion clicked")
+            btnNextQuestion.text = "Next question"
+            LOG.d("btnNextQuestion clicked")
+
             val question = questions.nextQuestion()
             txtOutput.text = question.text
+
+            answersList.adapter = ArrayAdapter<String>(this, R.layout.list_answer, question.answers.map { it.text })
+            answersList.setOnItemClickListener { parent, view, position, id ->
+                LOG.d("onItemClickListener on position: $position, id: $id, view: $view")
+            }
         }
     }
 
---
+++ app/src/main/kotlin/yobu/christophpickl/github/com/yobu/infrastructure.kt
@@ -0,0 +1,16 @@
+package yobu.christophpickl.github.com.yobu
+
+import android.util.Log
+
+fun LOG(javaClass: Class<Any>) = Log2(javaClass)
+
+class Log2(javaClass: Class<Any>) {
+    private val tag = javaClass.simpleName
+    fun d(message: String) {
+        Log.d(tag, message)
+    }
+
+    fun i(message: String) {
+        Log.i(tag, message)
+    }
+}
